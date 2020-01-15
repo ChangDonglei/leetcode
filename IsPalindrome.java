abadada3f0b08e4011e8160f373d887b38d64285
RayChang <cdljxqc@163.com>
2020-01-15 19:32:25 +0800

    Update IsPalindrome.java

--- IsPalindrome.java
+++ IsPalindrome.java
@@ -20,3 +20,32 @@
         }
         return res == x;
     }
+
+/**
+     * use list
+     *
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome_list(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x < 10) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; ; i *= 10) {
+            int cur = x % (i * 10) / i;
+            list.add(cur);
+            if (x / (i * 10) < 10) {// the highest number
+                list.add(x / (i * 10));
+                break;
+            }
+        }
+        for (int j = 0; j < list.size() / 2; j++) {
+            if (list.get(j) != list.get(list.size() - 1 - j)) {
+                return false;
+            }
+        }
+        return true;
+    }
